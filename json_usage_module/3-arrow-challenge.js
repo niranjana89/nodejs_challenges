e13cc0a7bf83615f17fea9b6acee758281f23a8f
niranjana89 <30704985+niranjana89@users.noreply.github.com>
2021-02-06 22:51:53 +0530

    Create 3-arrow-challenge.js

---
+++ json_usage_module/3-arrow-challenge.js
@@ -0,0 +1,19 @@
+const tasks = {
+  tasks: [
+    {
+      text: 'Grocery shopping',
+      completed: true,
+    },
+    {
+      text: 'Clean yard',
+      completed: false,
+    },
+    {
+      text: 'Film course',
+      completed: false,
+    },
+  ],
+  tasksTodo() {
+    return this.tasks.filter((task) => task.completed == false)
+  },
+}
